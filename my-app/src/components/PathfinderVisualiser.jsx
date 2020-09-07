e79c2436c57ff454e97f75c6ae2967723c1c0b61
Criscoll <cristiand2021@gmail.com>
2020-09-07 14:03:13 +1000

    Project repository initialisation

---
+++ my-app/src/components/PathfinderVisualiser.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class PathfinderVisualiser extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div>
+        <h1>Pathfinder Project</h1>
+      </div>
+    );
+  }
+}
+
+export default PathfinderVisualiser;
---
+++ my-app/src/components/node/node.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import "./node.css";
+
+class Node extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div>
+        <div class="grid-container">
+          <div class="grid-item">1</div>
+          <div class="grid-item">2</div>
+          <div class="grid-item">3</div>
+          <div class="grid-item">4</div>
+          <div class="grid-item">5</div>
+          <div class="grid-item">6</div>
+          <div class="grid-item">7</div>
+          <div class="grid-item">8</div>
+          <div class="grid-item">9</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Node;
