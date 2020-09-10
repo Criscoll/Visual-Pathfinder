a40c19b84f7b54eadbaf9ed9fcf40e4f17392755
Criscoll <cristiand2021@gmail.com>
2020-09-10 14:16:08 +1000

    hover event handlers for grid buttons added

--- my-app/src/components/header/header.jsx
+++ my-app/src/components/header/header.jsx
@@ -2,24 +2,70 @@ import React, { Component } from "react";
 import "./header.css";
 
 class Header extends Component {
-  state = {
-    nodes: [],
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      nodes: [],
+      isStartHovered: false,
+      isEndHovered: false,
+      isWallHovered: false,
+    };
+    this.handleEventStartHover = this.handleEventStartHover.bind(this);
+    this.handleEventEndHover = this.handleEventEndHover.bind(this);
+    this.handleEventWallHover = this.handleEventWallHover.bind(this);
+  }
+
+  handleEventStartHover() {
+    this.setState({ isStartHovered: !this.state.isStartHovered });
+  }
+
+  handleEventEndHover() {
+    this.setState({ isEndHovered: !this.state.isEndHovered });
+  }
+
+  handleEventWallHover() {
+    this.setState({ isWallHovered: !this.state.isWallHovered });
+  }
 
   render() {
+    const startBtnClass = this.state.isStartHovered
+      ? "button-hovered"
+      : "button-default";
+    const endBtnClass = this.state.isEndHovered
+      ? "button-hovered"
+      : "button-default";
+    const wallBtnClass = this.state.isWallHovered
+      ? "button-hovered"
+      : "button-default";
+
     return (
       <div>
         <h1>Visual Pathfinder</h1>
-        <ul className="node-selection">
-          <li className="node-type" onClick={this.props.startClick}>
+        <ul className="button-container">
+          <button
+            className={startBtnClass}
+            onClick={this.props.startClick}
+            onMouseEnter={this.handleEventStartHover}
+            onMouseLeave={this.handleEventStartHover}
+          >
             Start
-          </li>
-          <li className="node-type" onClick={this.props.endClick}>
+          </button>
+          <button
+            className={endBtnClass}
+            onClick={this.props.endClick}
+            onMouseEnter={this.handleEventEndHover}
+            onMouseLeave={this.handleEventEndHover}
+          >
             End
-          </li>
-          <li className="node-type" onClick={this.props.wallClick}>
+          </button>
+          <button
+            className={wallBtnClass}
+            onClick={this.props.wallClick}
+            onMouseEnter={this.handleEventWallHover}
+            onMouseLeave={this.handleEventWallHover}
+          >
             Wall
-          </li>
+          </button>
         </ul>
       </div>
     );
