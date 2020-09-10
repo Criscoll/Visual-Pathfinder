5d5b90750fe0f1bc1d1c2cca1a3f56ea7ad4c61a
Criscoll <cristiand2021@gmail.com>
2020-09-10 16:07:53 +1000

    header button clicking css styling added

--- my-app/src/components/header/header.jsx
+++ my-app/src/components/header/header.jsx
@@ -9,10 +9,15 @@ class Header extends Component {
       isStartHovered: false,
       isEndHovered: false,
       isWallHovered: false,
+      buttonPressed: "",
     };
     this.handleEventStartHover = this.handleEventStartHover.bind(this);
     this.handleEventEndHover = this.handleEventEndHover.bind(this);
     this.handleEventWallHover = this.handleEventWallHover.bind(this);
+
+    this.handleEventStartClick = this.handleEventStartClick.bind(this);
+    this.handleEventEndClick = this.handleEventEndClick.bind(this);
+    this.handleEventWallClick = this.handleEventWallClick.bind(this);
   }
 
   handleEventStartHover() {
@@ -27,40 +32,45 @@ class Header extends Component {
     this.setState({ isWallHovered: !this.state.isWallHovered });
   }
 
-  render() {
-    const startBtnClass = this.state.isStartHovered
-      ? "button-hovered"
-      : "button-default";
-    const endBtnClass = this.state.isEndHovered
-      ? "button-hovered"
-      : "button-default";
-    const wallBtnClass = this.state.isWallHovered
-      ? "button-hovered"
-      : "button-default";
+  handleEventStartClick() {
+    this.props.startClick();
+    this.setState({ buttonPressed: "start" });
+  }
 
+  handleEventEndClick() {
+    this.props.endClick();
+    this.setState({ buttonPressed: "end" });
+  }
+
+  handleEventWallClick() {
+    this.props.wallClick();
+    this.setState({ buttonPressed: "wall" });
+  }
+
+  render() {
     return (
       <div>
         <h1>Visual Pathfinder</h1>
         <ul className="button-container">
           <button
-            className={startBtnClass}
-            onClick={this.props.startClick}
+            className={this.getBtnClass("start")}
+            onClick={this.handleEventStartClick}
             onMouseEnter={this.handleEventStartHover}
             onMouseLeave={this.handleEventStartHover}
           >
             Start
           </button>
           <button
-            className={endBtnClass}
-            onClick={this.props.endClick}
+            className={this.getBtnClass("end")}
+            onClick={this.handleEventEndClick}
             onMouseEnter={this.handleEventEndHover}
             onMouseLeave={this.handleEventEndHover}
           >
             End
           </button>
           <button
-            className={wallBtnClass}
-            onClick={this.props.wallClick}
+            className={this.getBtnClass("wall")}
+            onClick={this.handleEventWallClick}
             onMouseEnter={this.handleEventWallHover}
             onMouseLeave={this.handleEventWallHover}
           >
@@ -69,6 +79,34 @@ class Header extends Component {
         </ul>
       </div>
     );
+  }
+
+  getBtnClass(button) {
+    let btnClass = "";
+
+    if (button === "start") {
+      btnClass = this.state.isStartHovered
+        ? "button-hovered"
+        : "button-default";
+
+      if (this.state.buttonPressed === "start") {
+        btnClass = "button-clicked";
+      }
+    } else if (button === "end") {
+      btnClass = this.state.isEndHovered ? "button-hovered" : "button-default";
+
+      if (this.state.buttonPressed === "end") {
+        btnClass = "button-clicked";
+      }
+    } else {
+      btnClass = this.state.isWallHovered ? "button-hovered" : "button-default";
+
+      if (this.state.buttonPressed === "wall") {
+        btnClass = "button-clicked";
+      }
+    }
+
+    return btnClass;
   }
 }
 
