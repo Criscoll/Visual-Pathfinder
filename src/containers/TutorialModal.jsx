ec6c4975b2b35f705b0a8d7b5e2a7675aa2b3ee7
Criscoll <cristiand2021@gmail.com>
2021-01-24 16:14:46 +1100

    tutorial modal in progress

--- src/containers/TutorialModal.jsx
+++ src/containers/TutorialModal.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDom from 'react-dom';
 import AppLogo from '../images/pathfinder-logo.svg';
+import { AiOutlineRight, AiOutlineLeft } from 'react-icons/ai';
 
 export default function TutorialModal() {
+  const [pageNum, setPageNum] = useState(1);
+
   return ReactDom.createPortal(
     <React.Fragment>
       <div className="modal-overlay">
@@ -12,15 +15,21 @@ export default function TutorialModal() {
             e.stopPropagation();
           }}
         >
-          <h1>Welcome to the Visual Pathfinder Tool! </h1>
-          <p>
-            This short tutorial will help you get started with the application
-            <br />
-            <br />
-            If at any point this gets too boring to read feel free to close this
-            tutorial to jump right in!
-          </p>
-          <img src={AppLogo} alt="logo" height="350"></img>
+          <div className="page-number">{pageNum}/9</div>
+          <AiOutlineRight className="arrow-icons right" />
+          <AiOutlineLeft className="arrow-icons left" />
+
+          <div className="modal-content">
+            <h1>Welcome to the Visual Pathfinder Tool! </h1>
+            <p>
+              This short tutorial will help you get started with the application
+              <br />
+              <br />
+              If at any point this gets too boring to read feel free to close
+              this tutorial to jump right in!
+            </p>
+            <img src={AppLogo} alt="logo" height="350"></img>
+          </div>
         </div>
       </div>
     </React.Fragment>,
