034d78f182c6079977b42630fadef18e16a01a83
Criscoll <cristiand2021@gmail.com>
2021-01-28 15:50:29 +1100

    build version created

---
+++ build/precache-manifest.b993cba672725864092b0589fd32cea6.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63005e87a1560dac17bed60a11c59249",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7814d287a1aa049452ef",
+    "url": "/static/css/2.28f0137e.chunk.css"
+  },
+  {
+    "revision": "7755f8c35aaf1e5c886d",
+    "url": "/static/css/main.73237fc2.chunk.css"
+  },
+  {
+    "revision": "7814d287a1aa049452ef",
+    "url": "/static/js/2.b82015d6.chunk.js"
+  },
+  {
+    "revision": "fde5ff8f4122a466397a13ef98d4f94d",
+    "url": "/static/js/2.b82015d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7755f8c35aaf1e5c886d",
+    "url": "/static/js/main.d5c6145e.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/static/media/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "cf35fe732ea1be5d7a2631ed1754708e",
+    "url": "/static/media/Padauk-Regular.cf35fe73.ttf"
+  },
+  {
+    "revision": "972d54ac7985dbc9a8014a49a29c7668",
+    "url": "/static/media/ShareTechMono-Regular.972d54ac.ttf"
+  },
+  {
+    "revision": "e00e2a77dd88a8fe75573a5d993af76a",
+    "url": "/static/media/Ubuntu-Bold.e00e2a77.ttf"
+  },
+  {
+    "revision": "8e22c2a6e3a3c679787e763a97fa11f7",
+    "url": "/static/media/Ubuntu-Medium.8e22c2a6.ttf"
+  },
+  {
+    "revision": "2505bfbd9bde14a7829cc8c242a0d25c",
+    "url": "/static/media/Ubuntu-Regular.2505bfbd.ttf"
+  },
+  {
+    "revision": "ed7d9d2d3213eecb0bc45b35ce02888f",
+    "url": "/static/media/circuit-board.ed7d9d2d.svg"
+  },
+  {
+    "revision": "ce3e85ff5ae29749713fbbf45140f8dc",
+    "url": "/static/media/moving-nodes.ce3e85ff.gif"
+  },
+  {
+    "revision": "68ad7b6967dae02cfb5fed50e3d2a8fb",
+    "url": "/static/media/pathfinder-logo.68ad7b69.svg"
+  },
+  {
+    "revision": "1e9b9135fa24887406241618126a74f0",
+    "url": "/static/media/pathfinding.1e9b9135.gif"
+  },
+  {
+    "revision": "a675e19b3b319fcb081bf5b04bcba9aa",
+    "url": "/static/media/placing-obstacles.a675e19b.gif"
+  }
+]);
